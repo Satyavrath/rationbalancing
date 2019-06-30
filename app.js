fca4baf631113673ba3f37abcc0fc96f16849772
satyavrath <satyavrathinjamuri87@gmail.com>
2019-06-30 13:12:45 -0500

    Added routes to provide entry point to the application. Updated node modules, added comments where required

--- app.js
+++ app.js
@@ -3,24 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// import index file from route folder
+var indexRouter = require('./routes/index')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// entry point to route
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("cattleweight.ejs");
 });
 
+router.get('/cattleweight', function(req, res, next) {
+  res.render("cattleweight.ejs");
+});
+//  GET feedstuff page on click
+router.get('/feedstuff', function(req, res, next) {
+  res.render("feedstuff.ejs");
+});
+//  GET quantities page on click
+router.get('/quantities', function(req, res, next) {
+  res.render("quantities.ejs");
+});
+//  GET rationweights page on click
+router.get('/rationweights', function(req, res, next) {
+  res.render("rationweights.ejs");
+});
+
+// exports router componets outside the folder for access
 module.exports = router;
