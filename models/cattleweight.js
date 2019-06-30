764b4319f9941e6581481a48db06ff0384e2b638
satyavrath <satyavrathinjamuri87@gmail.com>
2019-06-30 00:40:57 -0500

    Added cattleweight schema for storing cattleweights and weight gain. :

---
+++ models/cattleweight.js
@@ -0,0 +1,26 @@
+// cattleweight schema for storing cattle weights and weight gain
+
+const mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+const nutrientSchema = new mongoose.Schema({
+    NEg:Number,
+    Mp:Number,
+    Ca:Number,
+    p:Number
+
+});
+const weightGain = new mongoose.Schema({
+    gain: Number,
+    nutrients: [nutrientSchema]
+
+});
+const cattleWeightSchema = new mongoose.Schema({
+    _id: Number,
+    cattleWeight: Number,
+    gainPerDay:[weightGain]
+
+})
+
+module.exports = mongoose.model('cattleWeight',cattleWeightSchema);
