0401350f1f5241ee152decb053c9588d7b108cb3
karthik <s533488@nwmissouri.edu>
2019-11-10 16:48:18 -0600

    Java script function To avoid duplicate entry and to display in selected ingredients in table-format

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -25,3 +25,46 @@ $(document).ready(function () {
   });
 });
 
+// To avoid duplicate entry and to display in selected ingredients in table-format
+function myScript(e) {
+
+  let ings = e.target.attributes.id.ownerElement.innerHTML
+
+  ingredients.push(ings);
+  customId = ings.replace(/[ ,.%()]/g, "");
+  var tableData = document.getElementsByTagName("td");
+  for (var i = 0; i < tableData.length; i += 4) {
+    if (tableData[i].innerHTML == ings) {
+      alert("You cant add ingredients twice");
+      return;
+    }
+  }
+
+  // to display in selected ingredients
+  $('#table-style').append(
+    '<tr>' +
+    '<td id=' + customId + '>' + e.target.attributes.id.ownerElement.innerHTML + '</td>' +
+    '<td> <input type="text" class="form-control text-field-button" id=' + customId + "Quantity" + ' value=""> </td>' +
+    '<td><input type="text" class="form-control text-field-button" id=' + customId + "Cost" + ' value=""></td>' +
+    '<td><img src="../images/delete.jpeg" alt="Delete" style="width:30px;height:30px;"></td>' +
+    '</tr>'
+  );
+  //  To delete selected rows
+  var index, tab = document.getElementById("table-style");
+  for (var i = 1; i < tab.rows.length; i++) {
+    tab.rows[i].cells[3].onclick = function () {
+      index = this.parentElement.rowIndex;
+      tab.deleteRow(index);
+      ingredients.splice(index - 1, 1);
+      if (tab.rows.length == 1) {
+        $("#calculateBtn").hide();
+      }
+    }
+  }
+  if (tab.rows.length == 1) {
+    $("#calculateBtn").hide();
+  }
+  else {
+    $("#calculateBtn").show();
+  }
+}
