1bc7da15d5875dddeb61386ce5259fdbce378d10
sanjanaminnu <sanjanareddyminnu@gmail.com>
2019-11-10 19:33:57 -0600

    Added parser function

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -967,5 +967,79 @@ function calculateBTN() {
    
      }
 
+
+     function parser (InString, Sep)  {
+    
+       var NumSeps=0; var Count = 0;
+    
+       var location = new Array;
+    
+       location[0] = -1;
+    
+       var len = InString.length;
+    
+       for (Count=0; Count < len; Count++)  {
+    
+         if (InString.charAt(Count)==Sep)
+    
+           {
+    
+           NumSeps++;
+    
+           location[NumSeps] = Count;
+    
+           }
+    
+         }
+    
+       var parse = new makeArray (NumSeps+2);
+    
+       if (NumSeps == 0) {parse[0] = 1; parse[1] = InString; return(parse);}
+    
+       parse[0] = NumSeps + 1;
+
+       for (var i = 1; i <=NumSeps; i++)
+    
+         {
+    
+         parse[i] = InString.substring(location[i-1]+1, location[i]);
+         }
+    
+         parse[NumSeps+1] = InString.substring(location[NumSeps]+1, len);
+    
+       return (parse);
+    
+      }
+ 
+      function parseLinearExpr(InString) {
+    
+      InString = stripChar(InString,"(");   
+    
+      InString = stripChar(InString,")");
+    
+      var stringlen = InString.length
+    
+      if (!looksLikeANumber(InString.charAt(0))) InString = "1" + InString;
+    
+      if (InString.charAt(0) != "-") InString = "+"+ InString;
+    
+       var variableList = "";
+    
+       InString = replaceSubstring (InString,"+","_+");
+    
+       InString = replaceSubstring (InString,"-","_-");
+ 
+       var ch = "_";
+    
+       var Ar = parser(InString, ch);
+    
+       var parsd = new makeArray (Ar[0]+1, "");    
+       for (var i = 1; i < Ar[0]; i++)
+    
+         {
+    
+         parsd[i] = stripChar(Ar[i+1],"_");
+    
+         }
   
 }
